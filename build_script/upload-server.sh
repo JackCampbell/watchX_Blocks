424983c7dbd845edb72725a2c8733ba0cdcdc0ee
JackCampbell <jack_campbell_512@hotmail.com>
2022-07-20 14:17:30 +0300

    fixed process infomation

--- build_script/upload-server.sh
+++ build_script/upload-server.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
-scp "../dist/watchX Blocks-1.0.1028.dmg" root@watchx.io:/root/w-site/media/latest.dmg
-scp "../dist/watchX Blocks-1.0.1028-win.zip" root@watchx.io:/root/w-site/media/latest.zip
+scp "../dist/watchX Blocks-1.0.1666.dmg" root@watchx.io:/root/w-site/media/latest.dmg
+scp "../dist/watchX Blocks Setup 1.0.1666.exe" root@watchx.io:/root/w-site/media/latest.exe
+#scp "../dist/watchX Blocks-1.0.1028-win.zip" root@watchx.io:/root/w-site/media/latest.zip
 
